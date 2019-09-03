b01c4516ed38e9a1c48a448901131e70edf1dc6c
nephitejnf <nephitejnf@gmail.com>
2019-09-02 18:37:56 -0600

    Initial push, mostly working

---
+++ README.md
@@ -0,0 +1,5 @@
+# Muse2pokecrystal
+This is merely a script that takes musicxml and converts it to music code for pokecrystal. Musicxml is exported by programs such as musescore.
+
+The script expects four different channels. Two square sound channels, one wave channel (of some sort (sine/saw)), and a drumset channel.
+
---
+++ muse2pokecrystal.py
@@ -0,0 +1,246 @@
+#!/usr/bin/python3
+
+from xml.etree.ElementTree import parse
+import xml.etree.ElementTree as et
+import sys, getopt
+
+asmfile = None
+# <part-list><score-part id=""><part-name>Name</partname></score-part></part-list>
+# <part id=""><measure number="1"><notes /></measure></part>
+def process_score(xmlfile, musicfile):
+    global asmfile
+    asmfile = open(musicfile, "w")
+    # (part id, part-name)
+    parts_list = []
+
+    ScoreTree = parse(xmlfile)
+    xmlroot = ScoreTree.getroot()
+    song_title = xmlroot.find('./work/work-title').text
+    pointer_title = song_title.replace(':', '').replace(' ','')
+    parts = xmlroot.find('part-list')
+    for part in parts.findall('score-part'):
+        parts_list.append((part.get('id'), part.find('part-name').text))
+
+    print("adding Header info")
+    asmfile.write("Music_{}:\n".format(pointer_title))
+    asmfile.write("\tmusicheader 4, 1, Music_{}_Ch1\n".format(pointer_title))
+    asmfile.write("\tmusicheader 1, 2, Music_{}_Ch2\n".format(pointer_title))
+    asmfile.write("\tmusicheader 1, 3, Music_{}_Ch3\n".format(pointer_title))
+    asmfile.write("\tmusicheader 1, 4, Music_{}_Ch4\n\n\n".format(pointer_title))
+    print("Converting Channel 1: {}".format(parts_list[0][1]))
+    parse_channel1(xmlroot.find("./part[@id='{}']".format(parts_list[0][0])), pointer_title)
+    print("Converting Channel 2: {}".format(parts_list[1][1]))
+    parse_channel2(xmlroot.find("./part[@id='{}']".format(parts_list[1][0])), pointer_title)
+    print("Converting Channel 3: {}".format(parts_list[2][1]))
+    parse_channel3(xmlroot.find("./part[@id='{}']".format(parts_list[2][0])), pointer_title)
+    print("Converting Channel 4: {}".format(parts_list[3][1]))
+    parse_channel4(xmlroot.find("./part[@id='{}']".format(parts_list[3][0])), pointer_title)
+
+    # close
+    asmfile.close()
+
+
+def noise_process(pitch):
+    altered = pitch.find('alter')
+    step = pitch.find('display-step').text
+    if altered is not None: altered = altered.text
+    else: altered = '0'
+    nibble = '_'
+    if int(altered) < 0:
+        nibble = '#'
+        step = chr(ord(step)-1)
+    elif int(altered) > 0: nibble = '#'
+    noted = "{}{}".format(step, nibble)
+    return noted
+
+def note_process(pitch):
+    altered = pitch.find('alter')
+    step = pitch.find('step').text
+    if altered is not None: altered = altered.text
+    else: altered = '0'
+    nibble = '_'
+    if int(altered) < 0:
+        nibble = '#'
+        step = chr(ord(step)-1)
+    elif int(altered) > 0: nibble = '#'
+    noted = "{}{}".format(step, nibble)
+    return noted
+
+# tempo, volume, dutycycle, tone, vibrato, notetype, octave, stereopanning
+# <tie type="start" (type="stop")/>
+def parse_channel1(part, title):
+    global asmfile
+    # parse channel 1 header stuff
+    asmfile.write("Music_{}_Ch1:\n".format(title))
+    asmfile.write("\ttempo {}\n".format(int(19200/int(part.find('./measure/direction/sound').get('tempo')))))
+    asmfile.write("\tvolume $77\n")
+    asmfile.write("\tnotetype $c, $00\n")
+    asmfile.write("\tdutycycle $2\n")
+    asmfile.write("\tintensity $95\n")
+    asmfile.write("Music_{}_Ch1_Loop:\n".format(title))
+    curroctave = 4
+    asmfile.write("\toctave {}\n".format(curroctave))
+    tied = False
+    step = ''
+    dura = 0
+    # redundant note parsing code, note very efficient when copypastad
+    for measure in part.findall('measure'):
+        for note in measure.findall('note'):
+            if note.find('rest') is not None:
+                asmfile.write("\tnote __, {}\n".format(note.find('duration').text))
+            elif note.find('pitch') is None:
+                print('None?')
+            else:
+                pitch = note.find('pitch')
+                if int(pitch.find('octave').text) is not curroctave:
+                    curroctave = int(pitch.find('octave').text)
+                    asmfile.write("\toctave {}\n".format(curroctave))
+                if note.find('./tie[@type="start"]') is not None:
+                    tied = True
+                    step = note_process(pitch)
+                    dura = int(note.find('duration').text)
+                elif note.find('./tie[@type="stop"]') is not None:
+                    if step == note_process(pitch):
+                        t = "\tnote {}, {}\n".format(note_process(pitch),int(note.find('duration').text)+dura)
+                    else:
+                        asmfile.write("\tnote {}, {}\n".format(step,dura))
+                        t = "\tnote {}, {}\n".format(note_process(pitch),note.find('duration').text)
+                    tied = False
+                    step = ''
+                    dura = 0
+                else:
+                    t = "\tnote {}, {}\n".format(note_process(pitch),note.find('duration').text)
+                asmfile.write(t)
+    asmfile.write("\tloopchannel 0, Music_{}_Ch1_Loop\n\n\n".format(title))
+
+# dutycycle, tone, vibrato, notetype, octave, stereopanning
+def parse_channel2(part, title):
+    global asmfile
+    # default header stuff
+    asmfile.write("Music_{}_Ch2:\n".format(title))
+    asmfile.write("\tvolume $77\n")
+    asmfile.write("\tnotetype $c, $00\n")
+    asmfile.write("\tdutycycle $2\n")
+    asmfile.write("\tintensity $95\n")
+    asmfile.write("Music_{}_Ch2_Loop:\n".format(title))
+    curroctave = 4
+    asmfile.write("\toctave {}\n".format(curroctave))
+    tied = False
+    step = ''
+    dura = 0
+    # redundant note parsing code, note very efficient when copypastad
+    for measure in part.findall('measure'):
+        for note in measure.findall('note'):
+            if note.find('rest') is not None:
+                asmfile.write("\tnote __, {}\n".format(note.find('duration').text))
+            elif note.find('pitch') is None:
+                print('None?')
+            else:
+                pitch = note.find('pitch')
+                if int(pitch.find('octave').text) is not curroctave:
+                    curroctave = int(pitch.find('octave').text)
+                    asmfile.write("\toctave {}\n".format(curroctave))
+                if note.find('./tie[@type="start"]') is not None:
+                    print("Tied up")
+                    tied = True
+                    step = note_process(pitch)
+                    print(step)
+                    dura = int(note.find('duration').text)
+                elif note.find('./tie[@type="stop"]') is not None:
+                    if step == note_process(pitch):
+                        t = "\tnote {}, {}\n".format(note_process(pitch),int(note.find('duration').text)+dura)
+                    else:
+                        asmfile.write("\tnote {}, {}\n".format(step,dura))
+                        t = "\tnote {}, {}\n".format(note_process(pitch),note.find('duration').text)
+                    tied = False
+                    step = ''
+                    dura = 0
+                else:
+                    t = "\tnote {}, {}\n".format(note_process(pitch),note.find('duration').text)
+                asmfile.write(t)
+    asmfile.write("\tloopchannel 0, Music_{}_Ch2_Loop\n\n\n".format(title))
+
+# stereopanning, vibrato, notetype, tone, octave
+def parse_channel3(part, title):
+    global asmfile
+    asmfile.write("Music_{}_Ch3:\n".format(title))
+    asmfile.write("\tnotetype $c, $00\n")
+    asmfile.write("\tintensity $95\n")
+    asmfile.write("Music_{}_Ch3_Loop:\n".format(title))
+    # redundant note parsing code, note very efficient when copypastad
+    curroctave = 4
+    asmfile.write("\toctave {}\n".format(curroctave))
+    tied = False
+    step = ''
+    dura = 0
+    for measure in part.findall('measure'):
+        for note in measure.findall('note'):
+            if note.find('rest') is not None:
+                asmfile.write("\tnote __, {}\n".format(note.find('duration').text))
+            elif note.find('pitch') is None:
+                print('None?')
+            else:
+                pitch = note.find('pitch')
+                if int(pitch.find('octave').text) is not curroctave:
+                    curroctave = int(pitch.find('octave').text)
+                    asmfile.write("\toctave {}\n".format(curroctave))
+                if note.find('./tie[@type="start"]') is not None:
+                    tied = True
+                    step = note_process(pitch)
+                    dura = int(note.find('duration').text)
+                elif note.find('./tie[@type="stop"]') is not None:
+                    if step == note_process(pitch):
+                        t = "\tnote {}, {}\n".format(note_process(pitch),int(note.find('duration').text)+dura)
+                    else:
+                        asmfile.write("\tnote {}, {}\n".format(step,dura))
+                        t = "\tnote {}, {}\n".format(note_process(pitch),note.find('duration').text)
+                    tied = False
+                    step = ''
+                    dura = 0
+                else:
+                    t = "\tnote {}, {}\n".format(note_process(pitch),note.find('duration').text)
+                asmfile.write(t)
+    asmfile.write("\tloopchannel 0, Music_{}_Ch3_Loop\n\n\n".format(title))
+
+# notetype, togglenoise
+def parse_channel4(part, title):
+    global asmfile
+    asmfile.write("Music_{}_Ch4:\n".format(title))
+    asmfile.write("\tnotetype $c\n")
+    asmfile.write("\ttogglenoise 1\n")
+    asmfile.write("Music_{}_Ch4_Loop:\n".format(title))
+    # redundant note parsing code, note very efficient when copypastad
+    for measure in part.findall('measure'):
+        for note in measure.findall('note'):
+            if note.find('rest') is not None:
+                asmfile.write("\tnote __, {}\n".format(note.find('duration').text))
+            elif note.find('pitch') is not None:
+                pitch = note.find('pitch')
+                t = "\tnote {}, {}\n".format(note_process(pitch),note.find('duration').text)
+                asmfile.write(t)
+            elif note.find('unpitched') is not None:
+                pitch = note.find('unpitched')
+                t = "\tnote {}, {}\n".format(noise_process(pitch),note.find('duration').text)
+                asmfile.write(t)
+    asmfile.write("\tloopchannel 0, Music_{}_Ch4_Loop\n\n\n".format(title))
+
+def main(argv):
+    infile = ""
+    outfile = ""
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["score=","code="])
+    except getopt.GetoptError:
+        print('muse2pokecrystal -i <musicxml> -o <music code>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('muse2pokecrystal -i <musicxml> -o <music code>')
+            sys.exit()
+        elif opt in ("-i", "--score"):
+            infile = arg
+        elif opt in ("-o", "--code"):
+            outfile = arg
+    process_score(infile, outfile)
+
+if __name__=="__main__":
+    main(sys.argv[1:])
