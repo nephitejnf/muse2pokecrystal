58d2a7d0415b9c02e254d1d311e10fb8cbfb2f5b
Hyperdriveguy <hyperdriveguy@gmail.com>
2020-05-03 00:27:00 -0700

    Module docstring including license information

--- src/score.py
+++ src/score.py
@@ -1,3 +1,27 @@
+"""
+This module is a part of Muse2pokecrystal.
+
+This module contains the text for muse2pokecrystal.
+
+Copyright (C) 2020  nephitejnf and hyperdriveguy
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Full plain text license https://www.gnu.org/licenses/agpl-3.0.txt
+"""
+
+
 from xml.etree.ElementTree import parse
 from src import text, notes
 
