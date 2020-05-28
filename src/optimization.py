c8e86b42f7bb880c400bae6b90a7c1198416119e
Hyperdriveguy <hyperdriveguy@gmail.com>
2020-05-28 12:02:59 -0700

    At least it's something

--- src/optimization.py
+++ src/optimization.py
@@ -24,18 +24,19 @@ Full plain text license https://www.gnu.org/licenses/agpl-3.0.txt
 from random import random
 
 
-def callchannel_optimize(output_array):
+def callchannel_optimize(output_array, song_name):
     backup_array = output_array
-    search_array = []
-    search_match = True
-    match_index_array = [[]]
-    found_ideal_optimization = False
-    #cur_file_index = 0
-    # minimum search length is 4
-    for cur_file_index in range(0, len(output_array)):
+    branch = 1
+    cur_file_search_index = 0
+    while cur_file_search_index < len(output_array):
+        cur_file_search_index += 1
+        match_index_array = [[]]
+        found_ideal_optimization = False
+        # minimum search length is 4
         search_size = 4
         while not found_ideal_optimization:
-            for index in range(cur_file_index, search_size):
+            search_array = []
+            for index in range(cur_file_search_index, cur_file_search_index + search_size):
                 search_array.append(output_array[index])
             print(search_array)
             # check matches
@@ -44,13 +45,55 @@ def callchannel_optimize(output_array):
                     # we may have a match bois
                     search_match = True
                     for check_match_index in range(0, search_size):
-                        if(search_array[check_match_index] !=
-                           output_array[file_index + check_match_index]):
+                        try:
+                            if(search_array[check_match_index] !=
+                               output_array[file_index + check_match_index]):
+                                search_match = False
+                                break
+                        except IndexError:
                             search_match = False
                             break
                     if search_match:
-                        match_index_array.append(file_index)
-                        print(match_index_array)
+                        try:
+                            match_index_array[search_size - 4].append(file_index)
+                        except IndexError:
+                            match_index_array.append([])
+                            match_index_array[search_size - 4].append(file_index)
+            if len(match_index_array[-1]) <= 1:
+                # there's no other matches, so lets move on
+                opt_match = get_optimal_matches(match_index_array)
+                # no matches, move on
+                if opt_match is None:
+                    found_ideal_optimization = True
+                else:
+                    for matched_index in match_index_array[opt_match]:
+                        output_array.insert(matched_index,
+                                            'Music_' + song_name + '_Branch' + str(branch))
+                        for iteration in range(0, opt_match + 4):
+                            print(output_array.pop(matched_index + 1))
+                        print(output_array)
+                    # restart from the beginning
+                    cur_file_search_index = -1
+                    found_ideal_optimization = True
+                    branch += 1
+
+            else:
+                search_size += 1
+
+            #input()
+
+
+def get_optimal_matches(match_array):
+    optimum_index = None
+    for index in range(0, len(match_array)):
+        if (len(match_array[index]) >
+                len(match_array[index - 1])):
+            optimum_index = index
+    print(match_array)
+    print(optimum_index)
+    return optimum_index
+
+
 
 
 if __name__ == "__main__":
@@ -58,4 +101,4 @@ if __name__ == "__main__":
     for l in range(0, int(random() * 1000)):
         rand_list.append(str(round(random() * 10)) + '\n')
     print(rand_list)
-    callchannel_optimize(rand_list)
+    callchannel_optimize(rand_list, 'Yeet_Song')
